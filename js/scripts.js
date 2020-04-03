61de4cc1ba86650bc1664bdd871241687fb3740c
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-03 08:57:48 -0700

    add in initial Javascript.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function humanNumber(robotNumber) {
+  var ourResult="";
+}
+
+
+
+
+// User Interface
+
+$(document).ready(function() {
+  $("form#numberInputForm").submit(function(event() {
+    event.preventDefault();
+    $("#numberInputResults").text("");
+    var human = parse.Int($("#input").val());
+  }
+}
