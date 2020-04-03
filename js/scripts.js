f68096d806dbdf84bc5a34ddae7f1409c18346a9
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-03 12:17:00 -0700

    add font to header

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 function humanNumber(robotNumber) {
-var ourResult="";
+  var numArray=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var ourResult="";
 
   if (robotNumber === 0) {
+    
     ourResult = (ourResult + "COOL");
 
   } else if (robotNumber === 1) {
